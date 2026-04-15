aad4611fef5f1c8dc8d566044c2c7e72f395b9e0
nikhilsinghlom-sudo <nikhilsingh.lom@gmail.com>
2026-04-15 13:19:54 +0530

    Add files via upload

--- src/pages/Events/jec-world-2026.tsx
+++ src/pages/Events/jec-world-2026.tsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 // import { useNavigate } from 'react-router-dom';
 
 const EventDetail: React.FC = () => {
-//   const options: Plyr.Options = {
-//     controls: ["play-large"], // Custom controls
-//     settings: [], // Settings options
-//     autoplay: false, // Autoplay option
-//     // muted: false, // Mute video initially
-//     // clickToPlay: true
-//     hideControls: true,
-//   };
+  //   const options: Plyr.Options = {
+  //     controls: ["play-large"], // Custom controls
+  //     settings: [], // Settings options
+  //     autoplay: false, // Autoplay option
+  //     // muted: false, // Mute video initially
+  //     // clickToPlay: true
+  //     hideControls: true,
+  //   };
 
   // const navigate = useNavigate();
 
@@ -47,7 +47,18 @@ const EventDetail: React.FC = () => {
                   <div className="maintitle">
                     <h4>JEC WORLD 2026</h4>
                     <p>
-                      JEC World 2026 has been an inspiring platform for innovation and collaboration. Recyclamine continues to lead the conversation on sustainability, showcasing solutions that are reshaping the future of composites through stronger partnerships and shared ideas.
+                      At JEC World 2026, Recyclamine showcased its cutting-edge
+                      solutions for circular, sustainable composites. Our
+                      technology for recycling epoxy composites was on display,
+                      highlighting how we are enabling more sustainable
+                      practices in the industry. <br />
+                      <br />
+                      Rajesh Balakrishnan, CEO, Aditya Birla Group, shared
+                      insights on the importance of collaboration across the
+                      value chain to close the loop on circular composites. He
+                      stressed that success in sustainability requires bringing
+                      together OEMs, recyclers, and other stakeholders to invest
+                      in and work toward long-term solutions.
                     </p>
                   </div>
                 </aside>
@@ -117,12 +128,13 @@ const EventDetail: React.FC = () => {
           <div className="row">
             <div className="col-12 col-lg-4">
               <div className="applications-detail-lhs">
-                              <hr />
-              <p> Catack-H MoU signing</p>
-              <div className='backtopage'>
-                                  <Link to={"/events"}>&#60; Back</Link><br/>
-              {/* <button onClick={() => navigate('/', { state: { scrollTo: 'past-events' } })}>{'<'} Back</button> */}
-              </div>
+                <hr />
+                <p> Catack-H MoU signing</p>
+                <div className="backtopage">
+                  <Link to={"/events"}>&#60; Back</Link>
+                  <br />
+                  {/* <button onClick={() => navigate('/', { state: { scrollTo: 'past-events' } })}>{'<'} Back</button> */}
+                </div>
               </div>
             </div>
             <div className="col-12 col-lg-8 mt-5">
@@ -130,7 +142,16 @@ const EventDetail: React.FC = () => {
                 <aside>
                   <div className="maintitle">
                     <h4>Catack-H MoU signing</h4>
-                    
+                    <p>
+                      Recyclamine also took a significant step forward with
+                      Catack-H by signing a Memorandum of Understanding (MoU) to
+                      form a strategic partnership focused on the recycling of
+                      composites. This collaboration aims to develop scalable
+                      and commercially viable solutions to address End-of-Life
+                      waste in the composites industry, reinforcing both
+                      companies’ commitment to sustainable innovation and
+                      cross-border collaboration.
+                    </p>
                   </div>
                 </aside>
                 <aside>
